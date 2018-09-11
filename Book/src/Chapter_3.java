25c4821d903ab1a2664e507830e489f30d56fe3b
IvanJackson <ivan.jackson@upr.edu>
2018-09-10 23:30:40 -0400

    Same

--- Book/src/Chapter_3.java
+++ Book/src/Chapter_3.java
@@ -1,6 +1,3 @@
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class Chapter_3 {
@@ -9,17 +6,11 @@ public class Chapter_3 {
 		// TODO Auto-generated method stub
 		JFrame frame = new JFrame();
 		frame.setSize(300, 400);
-		frame.setTitle("Test Frame");
+		frame.setTitle("Two Rectangles");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		RectangleComponent component = new RectangleComponent();
+		frame.add(component);
 	}
-	public class RectangleComponent extends JComponent {
-		public void paintComponent(Graphics g)
-		{
-		// Recover Graphics2D 
-		Graphics2D g2 = (Graphics2D) g;
-		Rectangle box = new Rectangle(5, 10, 20, 30); 
-//		g2.draw(box);
-		} }
 
 }
